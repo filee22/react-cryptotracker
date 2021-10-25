89545b2dd337645895e8ca428de15011854735fb
filee22 <file.b225@gmail.com>
2021-10-25 18:56:45 +0200

    improved skeleton with different element sizes

--- src/CoinSkeleton.js
+++ src/CoinSkeleton.js
@@ -3,40 +3,212 @@ import './CoinSkeleton.css'
 
 const CoinSkeleton = () => {
   return (
-    <div className='coin-loader'>
-      <div className='coin-loader-container rank-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+    <div className='skeleton'>
+      <div className='coin-loader'>
+        <div className='role-cont rankload'>
+          <div className='coin-loader-container rank-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont nameload'>
+          <div className='coin-loader-container name-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont priceload'>
+          <div className='coin-loader-container price-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont changeload'>
+          <div className='coin-loader-container change-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont percload'>
+          <div className='coin-loader-container perc-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont mcapload'>
+          <div className='coin-loader-container mcap-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont volload'>
+          <div className='coin-loader-container vol-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='coin-loader-container name-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+
+      <div className='coin-loader'>
+        <div className='role-cont rankload'>
+          <div className='coin-loader-container rank-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont nameload'>
+          <div className='coin-loader-container name2-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont priceload'>
+          <div className='coin-loader-container price2-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont changeload'>
+          <div className='coin-loader-container change2-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont percload'>
+          <div className='coin-loader-container perc-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont mcapload'>
+          <div className='coin-loader-container mcap2-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont volload'>
+          <div className='coin-loader-container vol2-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='coin-loader-container price-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+
+      <div className='coin-loader'>
+        <div className='role-cont rankload'>
+          <div className='coin-loader-container rank-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont nameload'>
+          <div className='coin-loader-container name3-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont priceload'>
+          <div className='coin-loader-container price3-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont changeload'>
+          <div className='coin-loader-container change3-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont percload'>
+          <div className='coin-loader-container perc-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont mcapload'>
+          <div className='coin-loader-container mcap3-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont volload'>
+          <div className='coin-loader-container vol3-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='coin-loader-container change-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+
+      <div className='coin-loader'>
+        <div className='role-cont rankload'>
+          <div className='coin-loader-container rank-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='coin-loader-container perc-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+        <div className='role-cont nameload'>
+          <div className='coin-loader-container name4-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='coin-loader-container mcap-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+        <div className='role-cont priceload'>
+          <div className='coin-loader-container price4-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='coin-loader-container vol-load'>
-        <div className='coin-shimmer-wrapper'>
-          <div className='coin-shimmer'></div>
+        <div className='role-cont changeload'>
+          <div className='coin-loader-container change4-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont percload'>
+          <div className='coin-loader-container perc-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont mcapload'>
+          <div className='coin-loader-container mcap4-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
+        </div>
+        <div className='role-cont volload'>
+          <div className='coin-loader-container vol4-load'>
+            <div className='coin-shimmer-wrapper'>
+              <div className='coin-shimmer'></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
